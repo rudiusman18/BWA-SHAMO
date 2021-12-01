b48fd8bd56208616da0efa4fd1c2ad4ed0b3870e
rudi-usman <commander.heaven@gmail.com>
2021-12-01 22:44:25 +0700

    deploy image

--- resources/views/backend/admin/products/product.blade.php
+++ resources/views/backend/admin/products/product.blade.php
@@ -93,8 +93,7 @@
                                         Images:
                                         <div class="img">
                                             @foreach ($product->productgallery as $gallery)
-                                                <img class="img-fluid my-2"
-                                                    src="{{ getUrlAttribute($gallery->img_url) }}"
+                                                <img class="img-fluid my-2" src="/Storage/{{ $gallery->img_url }}"
                                                     alt="{{ $gallery->img_url }}">
 
                                             @endforeach
